2659302c6f349f0513bc1e7f163f515200fe2605
eliboa <eliboa@gmail.com>
2019-02-06 16:19:03 +0100

    Revert "Get storage info"
    This reverts commit 1b179f1fd2c1a6d80b2514178a346d5d9377b0ed.

--- NxNandManager/types.h
+++ NxNandManager/types.h
@@ -1,14 +1,4 @@
 #pragma once
 
-
-
-typedef unsigned long long int u64;
-
-typedef struct _NxStorage
-{
-    int type;
-    u64 size;
-} NxStorage;
-
 template<typename T, size_t ARR_SIZE>
 size_t array_countof(T(&)[ARR_SIZE]) { return ARR_SIZE; }
