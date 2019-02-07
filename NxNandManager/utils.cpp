5d71d900b4105e75537062a7e453da7a6086fada
eliboa <eliboa@gmail.com>
2019-02-07 13:07:41 +0100

    added : utils

---
+++ NxNandManager/utils.cpp
@@ -0,0 +1,64 @@
+#include "utils.h"
+using namespace std;
+
+wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
+{
+	wchar_t* wString = new wchar_t[4096];
+	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+	return wString;
+}
+
+LPWSTR convertCharArrayToLPWSTR(const char* charArray)
+{
+	int nSize = MultiByteToWideChar(CP_ACP, 0, charArray, -1, NULL, 0);
+	LPWSTR wString = new WCHAR[nSize];
+	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+	return wString;
+}
+
+
+u64 GetFilePointerEx (HANDLE hFile) {
+    LARGE_INTEGER liOfs={0};
+    LARGE_INTEGER liNew={0};
+    SetFilePointerEx(hFile, liOfs, &liNew, FILE_CURRENT);
+    return liNew.QuadPart;
+}
+
+unsigned long sGetFileSize(std::string filename)
+{
+	struct stat stat_buf;
+	int rc = stat(filename.c_str(), &stat_buf);
+	return rc == 0 ? stat_buf.st_size : -1;
+}
+
+std::string GetLastErrorAsString()
+{
+	//Get the error message, if any.
+	DWORD errorMessageID = ::GetLastError();
+	if (errorMessageID == 0)
+		return std::string(); //No error message has been recorded
+
+	LPSTR messageBuffer = NULL;
+	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+	std::string message(messageBuffer, size);
+
+	//Free the buffer.
+	LocalFree(messageBuffer);
+
+	return message;
+}
+
+
+constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+std::string hexStr(unsigned char *data, int len)
+{
+  std::string s(len * 2, ' ');
+  for (int i = 0; i < len; ++i) {
+    s[2 * i]     = hexmap[(data[i] & 0xF0) >> 4];
+    s[2 * i + 1] = hexmap[data[i] & 0x0F];
+  }
+  return s;
+}
---
+++ NxNandManager/utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <sys/types.h>
+#include <windows.h>
+#include <winioctl.h>
+#include "types.h"
+#include <sys/stat.h>
+
+wchar_t *convertCharArrayToLPCWSTR(const char* charArray);
+LPWSTR convertCharArrayToLPWSTR(const char* charArray);
+u64 GetFilePointerEx (HANDLE hFile);
+unsigned long sGetFileSize(std::string filename);
+std::string GetLastErrorAsString();
+std::string hexStr(unsigned char *data, int len);
