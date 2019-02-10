283e5bb1c80ae0df45b4211776e0dcbb5bafe54b
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2019-02-10 23:10:57 +0100

    Fix compilation error for sprintf_s() if the project is compiled under Mingw, using snprintf if it's the case.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- NxNandManager/utils.h
+++ NxNandManager/utils.h
@@ -8,6 +8,19 @@
 #include <sys/stat.h>
 #include <iostream>
 
+// MinGW
+#if defined(__MINGW32__) || defined(__MINGW64__)
+#   define _ELPP_MINGW 1
+#else
+#   define _ELPP_MINGW 0
+#endif // defined(__MINGW32__) || defined(__MINGW64__)
+// Some special functions that are special for VC++
+// This is to prevent CRT security warnings and to override deprecated methods but at the same time
+// MinGW does not support some functions, so we need to make sure that proper function is used.
+#if defined(_ELPP_MINGW)
+#   define sprintf_s snprintf
+#endif
+
 /*
 BOOL GetStorageInfo(LPWSTR storage, NxStorage* nxdata);
 BOOL ParseGpt(NxStorage* nxStorage, unsigned char *gptHeader);
