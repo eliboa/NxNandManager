23fbf4ea164a65147117f7dbcf3863ff5c109acf
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2025-07-17 03:23:03 +0200

    Add firmware 20.2.0 NCA identification.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- NxNandManager/NxStorage.cpp
+++ NxNandManager/NxStorage.cpp
@@ -66,6 +66,7 @@ static NxStorageType NxTypesArr[] =
 
 // Title ID 0100000000000809 (SystemVersion)
 static NxSystemTitles systemTitlesArr[] = {
+    { "20.2.0", "8fefd5281d05b908f6537ac080dd55b9.nca"},
     { "20.1.5", "5e67272eb312e5801f055fdbf9f12e39.nca"},
     { "20.1.1", "5cb1ecaa7a31fe7ba1c37836a030ccb0.nca"},
     { "20.1.0", "f58f9b84fba8df8227600d98cf326b5e.nca"},
@@ -143,6 +144,7 @@ static NxSystemTitles systemTitlesArr[] = {
 
 // Title ID 010000000000081B (BootImagePackageExFat)
 static NxSystemTitles exFatTitlesArr[] = {
+    { "20.2.0", "793b767dc1ded58a9d1922df07bc0cd4.nca"},
     { "20.1.5", "793b767dc1ded58a9d1922df07bc0cd4.nca"},
     { "20.1.1", "793b767dc1ded58a9d1922df07bc0cd4.nca"},
     { "20.1.0", "793b767dc1ded58a9d1922df07bc0cd4.nca"},
