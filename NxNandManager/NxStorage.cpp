b13e21fa9f95fc053051b49b739b9c6ae5401211
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2025-11-26 03:13:11 +0100

    Add firmware 21.0.1 NCAs identification.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- NxNandManager/NxStorage.cpp
+++ NxNandManager/NxStorage.cpp
@@ -66,6 +66,7 @@ static NxStorageType NxTypesArr[] =
 
 // Title ID 0100000000000809 (SystemVersion)
 static NxSystemTitles systemTitlesArr[] = {
+    { "21.0.1", "e7273dd5b560d0ba282fc64206fecb56.nca"},
     { "21.0.0", "4b0130c8b9d2174a6574f6247655acc0.nca"},
     { "20.5.0", "23ce01f1fc55e55a783162d456e5ca58.nca"},
     { "20.4.0", "c413a3965b7a3d4c9bfa5fb47a43dbcc.nca"},
@@ -148,6 +149,7 @@ static NxSystemTitles systemTitlesArr[] = {
 
 // Title ID 010000000000081B (BootImagePackageExFat)
 static NxSystemTitles exFatTitlesArr[] = {
+    { "21.0.1", "5d920340732acee21eda71743688d71a.nca"},
     { "21.0.0", "5d920340732acee21eda71743688d71a.nca"},
     { "20.5.0", "793b767dc1ded58a9d1922df07bc0cd4.nca"},
     { "20.4.0", "793b767dc1ded58a9d1922df07bc0cd4.nca"},
