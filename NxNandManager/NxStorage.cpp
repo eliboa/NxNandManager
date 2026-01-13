1b100cc2c5a5fd09b8d49052453d169fb20d4cae
impeeza <41979604+impeeza@users.noreply.github.com>
2026-01-12 21:15:21 -0500

    Add support to Firmware 21.2.0
    Add support to Firmware 21.2.0

--- NxNandManager/NxStorage.cpp
+++ NxNandManager/NxStorage.cpp
@@ -66,6 +66,7 @@ static NxStorageType NxTypesArr[] =
 
 // Title ID 0100000000000809 (SystemVersion)
 static NxSystemTitles systemTitlesArr[] = {
+    { "21.2.0", "ac1ef488027ac5d9222d74393f03507f.nca"},
     { "21.1.0", "738da79326689ef4b72f702693bfc48a.nca"},
     { "21.0.1", "e7273dd5b560d0ba282fc64206fecb56.nca"},
     { "21.0.0", "4b0130c8b9d2174a6574f6247655acc0.nca"},
@@ -150,6 +151,7 @@ static NxSystemTitles systemTitlesArr[] = {
 
 // Title ID 010000000000081B (BootImagePackageExFat)
 static NxSystemTitles exFatTitlesArr[] = {
+    { "21.2.0", "061e68902d7a8f431787428bd328a2fa.nca"},
     { "21.1.0", "5d920340732acee21eda71743688d71a.nca"},
     { "21.0.1", "5d920340732acee21eda71743688d71a.nca"},
     { "21.0.0", "5d920340732acee21eda71743688d71a.nca"},
